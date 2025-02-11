969611f2449ccda1656c7b92340f517a9c63a3ae
arojoMRI <rojoadrian16@gmail.com>
2025-02-11 13:54:52 -0600

    Basic HTML form layout

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 func main() {
+	// Serves static files i.e CSS + JS
 	serveStaticFiles()
 
+	// Routes located in routes.go
 	http.HandleFunc("/", getLanding)
-	port := ":8200"
 
+	//Server
+	port := ":8200"
 	fmt.Printf("Live at: http://localhost%v", port)
 	err := http.ListenAndServe(port, nil)
 	if err == nil {
-
+		panic(err)
 	}
 }
--- routes.go
+++ routes.go
@@ -4,12 +4,10 @@ import (
 	"net/http"
 )
 
-func getLanding(w http.ResponseWriter, r *http.Request) {
-
-	http.ServeFile(w, r, "./View/index.html")
-
-}
-
 func serveStaticFiles() {
 	http.Handle("/View/", http.StripPrefix("/View/", http.FileServer(http.Dir("./View/"))))
+}
+
+func getLanding(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./View/index.html")
 }
